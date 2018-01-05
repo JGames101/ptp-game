6751fdbcb9b2403a932fc1f7e4c908a634b1d172
JGames101 <jbmadden@hotmail.ca>
2018-01-05 10:31:14 -0800

    First commit

---
+++ javascript.js
@@ -0,0 +1,30 @@
+var peer = new Peer({key: 'lwjd5qra8257b9'});
+
+peer.on('open', function(id) {
+    var playerId = id;
+    document.querySelector('#idDisplay').innerHTML = playerId;
+});
+
+peer.on('connection', function(conn) {
+    console.log('I have been connected ' + conn);
+});
+
+document.querySelector('#hostGameButton').addEventListener("click",function(){
+    document.querySelector('#hostGame').className = 'open';
+    var role = 'host';
+});
+
+document.querySelector('#joinGameButton').addEventListener("click",function(){
+    document.querySelector('#joinGame').className = 'open';
+    var role = 'join';
+});
+
+document.querySelector('#joinFormIdButton').addEventListener("click",function(){
+    peerId = document.querySelector('#gameId').value;
+    var conn = peer.connect(peerId);
+});
+
+document.querySelector('#beginButton').addEventListener("click",function(){
+    document.querySelector('#titlePage').className = 'pageBlue right';
+    document.querySelector('#setNamePage').className = 'pageBlue open';
+});
